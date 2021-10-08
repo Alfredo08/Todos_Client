011463f9dbfa5935686e885cfc3f65ba8135b5a6
Alfredo08 <alfredosv08@gmail.com>
2021-10-08 12:00:57 -0500

    Adding POST and PUT

--- js/index.js
+++ js/index.js
@@ -66,5 +66,74 @@ function fetchUsers( event ){
         });
 }
 
+function addNewUser( event ){
+    event.preventDefault();
+    
+    let userForm = new FormData( event.target );
+    let URL = "http://127.0.0.1:5000/api/users/add";
+    let settings = {
+        method: 'POST',
+        body: userForm
+    };
+
+    fetch( URL, settings )
+        .then( response => {
+            if( response.ok ){
+                return response.json();
+            }
+        })
+        .then( data => {
+            console.log( data );
+            let results = document.querySelector( '.results' );
+            results.innerHTML += `
+                    <h2>
+                        ${data.username}
+
+                        <button class="deleteUser" data-username="${data.username}">
+                            Delete
+                        </button>
+                    </h2>
+                `;
+            let deleteButtons = document.querySelectorAll( '.deleteUser' );
+            deleteButtons[ deleteButtons.length - 1 ].addEventListener( 'click', deleteUser );
+        })
+        .catch( error => {
+            console.log( error );
+        });
+}
+
+function updateUserPassword( event ){
+    event.preventDefault();
+
+    let data = {
+        username: event.target.username.value,
+        password: event.target.password.value
+    };
+    let URL = "http://127.0.0.1:5000/api/users/update";
+    let settings = {
+        method: 'PUT',
+        body: JSON.stringify( data )
+    };
+
+    fetch( URL, settings )
+        .then( response => {
+            if( response.ok ){
+                return response.json();
+            }
+        })
+        .then( result => {
+            console.log( result );
+        })
+        .catch( error => {
+            console.log( error );
+        });
+}
+
 let loadUserButton = document.querySelector( '.loadUsers' );
 loadUserButton.addEventListener( 'click', fetchUsers );
+
+let addUserForm = document.querySelector( "#addUser" );
+addUserForm.addEventListener( 'submit', addNewUser);
+
+let updateUserPasswordForm = document.querySelector( "#updateUserPassword" );
+updateUserPasswordForm.addEventListener( 'submit', updateUserPassword )
